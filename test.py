a785986fa94e499a5801c4dbd845c7fd43b1b852
Owen Schandle <owen.schandle@gmail.com>
2022-02-12 01:52:50 -0500

    Basic skeleton to test Github Actions

---
+++ test.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+
+siteDir = os.path.join(os.getcwd(), "site")
+
+if (os.path.isdir(siteDir)):
+    raise Exception("You don't want to be running this script with an existing site.")
+os.mkdir(siteDir)
+
+# -------------------------------------
+print("Generating test django site...")
+# -------------------------------------
+uid = subprocess.check_output(["id","-u"]).rstrip().decode("UTF-8")
+gid = subprocess.check_output(["id","-g"]).rstrip().decode("UTF-8")
+
+# We could just generate the site on the local machine, but doing it in a
+# container ensures that our version of django matches.
+subprocess.check_call(f"docker build -f environments/site/Dockerfile site -t tmp-deleteme --build-arg UID={uid} --build-arg GID={gid}", shell=True)
+subprocess.check_call(f"docker run --user={uid}:{gid} -v {siteDir}:/var/www -it tmp-deleteme bash -c \"cd /var/www && pip install django && django-admin startproject testsite\"", shell=True)
+
+# Cleanup
+os.rmdir(siteDir)
