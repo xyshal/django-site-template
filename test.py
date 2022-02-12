6a3b156c8e457f8c4f1ea964c0c80b1c74e5f633
Owen Schandle <aheretic@gmail.com>
2022-02-12 02:03:56 -0500

    Of course, the build system has no tty

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-import subprocess
 import os
+import shutil
+import subprocess
 
 siteDir = os.path.join(os.getcwd(), "site")
 
@@ -16,7 +17,7 @@ gid = subprocess.check_output(["id","-g"]).rstrip().decode("UTF-8")
 # We could just generate the site on the local machine, but doing it in a
 # container ensures that our version of django matches.
 subprocess.check_call(f"docker build -f environments/site/Dockerfile site -t tmp-deleteme --build-arg UID={uid} --build-arg GID={gid}", shell=True)
-subprocess.check_call(f"docker run --user={uid}:{gid} -v {siteDir}:/var/www -it tmp-deleteme bash -c \"cd /var/www && pip install django && django-admin startproject testsite\"", shell=True)
+subprocess.check_call(f"docker run --user={uid}:{gid} -v {siteDir}:/var/www tmp-deleteme bash -c \"cd /var/www && pip install django && django-admin startproject testsite\"", shell=True)
 
 # Cleanup
-os.rmdir(siteDir)
+shutil.rmtree(siteDir)
