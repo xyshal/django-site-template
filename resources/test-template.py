e26410785469f1316a15d823a49d9510fcfc21de
Owen Schandle <aheretic@gmail.com>
2022-02-18 12:44:17 -0500

    Split the tests into a separate file, so that the logic of testing the template is distinct from the logic of testing the production site.

--- resources/test-template.py
+++ resources/test-template.py
@@ -75,50 +75,10 @@ subprocess.check_call("docker-compose up -d", shell=True)
 # `docker compose up -d --wait`
 time.sleep(10)
 
-# TODO: Maybe this here is the time to split the file; put this file in resources and call files in the root that would actually be used in production.
-
-# TODO: Variable-ize "site:latest"
-container = ""
-containers = subprocess.check_output(["docker","container", "ls"]).rstrip().decode("UTF-8")
-for line in containers.split("\n"):
-    if "site:latest" in line:
-        container = line.split(" ")[0]
-assert container != ""
-
 # -----------------------
 print("Running tests...")
 # -----------------------
-# Run a sanity check
-requestOutput = "testindex.html"
-requestCmd = ""
-if (shutil.which("wget") is not None):
-  requestCmd = f"wget -O {requestOutput} http://127.0.0.1:8000"
-elif (shutil.which("curl") is not None):
-  requestCmd = f"curl -o {requestOutput} http://127.0.0.1:8000 --connect-timeout 5"
-else:
-  # TODO: Use the python requests library in this case?
-  raise Exception("Don't know how to make a request")
-
-if (os.path.exists(requestOutput)):
-    os.remove(requestOutput)
-subprocess.check_call(requestCmd, shell=True)
-os.remove(requestOutput)
-
-# Run a unit test (through a docker exec?)
-subprocess.check_call(f"docker exec {container} /bin/bash -c \"cd /var/www && python3 manage.py test\"", shell=True)
-
-# TODO: Run an integration test.
-# TODO: This takes a long time; should we provide a mechanism to cache this container?
-subprocess.check_call(f"docker cp environments/site/install-test-dependencies.sh {container}:/root", shell=True)
-subprocess.check_call(f"docker cp environments/site/launch-vnc.sh {container}:/var/www", shell=True)
-subprocess.check_call(f"docker exec --user root {container} /bin/bash /root/install-test-dependencies.sh", shell=True)
-
-subprocess.check_call(f"docker exec -d {container} /bin/bash /var/www/launch-vnc.sh", shell=True)
-subprocess.check_call(f"docker exec {container} pip3 install selenium", shell=True)
-
-subprocess.check_call(f"docker cp test/integration-test.py {container}:/var/www", shell=True)
-subprocess.check_call(f"docker exec {container} /bin/bash -c \"DISPLAY=:0 python3 /var/www/integration-test.py\"", shell=True)
-
+subprocess.check_call("python3 runtests.py", shell=True)
 
 # ---------------------
 print("Cleaning up...")
