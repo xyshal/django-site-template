10f087923c7fc391590f9f3a7e77b49c3f7f14a8
Owen Schandle <aheretic@gmail.com>
2022-02-20 04:00:34 -0500

    Added support for ARM64 Linux builds
    This is done by grabbing an Ubuntu ARM64 geckodriver package and
    extracting its binary :grimacing:

--- environments/site/install-test-dependencies.sh
+++ environments/site/install-test-dependencies.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 apt-get update && apt-get install -yf firefox-esr tigervnc-standalone-server openbox
-wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz
-tar xf geckodriver-v0.30.0-linux64.tar.gz -C /usr/bin
+
+uname -a | grep -q arm64
+if [ $? -eq 0 ]; then
+  wget http://launchpadlibrarian.net/583757436/firefox-geckodriver_97.0+build2-0ubuntu0.18.04.1_arm64.deb
+  ar x firefox-geckodriver_97.0+build2-0ubuntu0.18.04.1_arm64.deb
+  tar xf data.tar.xz
+else
+  wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz
+  tar xf geckodriver-v0.30.0-linux64.tar.gz -C /usr/bin
+fi
