bbda5e28897d57e28ab032860da727596aae7945
Owen Schandle <aheretic@gmail.com>
2022-02-20 15:26:40 -0500

    Removed the step where we create the site with the test script.
    It's becoming more trouble than I think it's worth to copy site
    resources into the second execution of the Dockerfile.  There's for sure
    some benefit to creating the site with the same version of django as is
    running the site, so perhaps this should be improved upon later.
    
    No behavior changes are intended beyond the removal of the site
    generation.
    
    Changes to be committed:
    	modified:   .gitignore
    	modified:   resources/test-template.py
    	new file:   testsite/manage.py
    	new file:   testsite/testapp/__init__.py
    	new file:   testsite/testapp/admin.py
    	new file:   testsite/testapp/apps.py
    	new file:   testsite/testapp/migrations/__init__.py
    	renamed:    resources/site/testapp-models.py -> testsite/testapp/models.py
    	renamed:    resources/site/styles.css -> testsite/testapp/static/css/styles.css
    	renamed:    resources/site/base_generic.html -> testsite/testapp/templates/base_generic.html
    	renamed:    resources/site/index.html -> testsite/testapp/templates/index.html
    	renamed:    resources/site/testapp-tests.py -> testsite/testapp/tests.py
    	renamed:    resources/site/testapp-urls.py -> testsite/testapp/urls.py
    	renamed:    resources/site/testapp-views.py -> testsite/testapp/views.py
    	new file:   testsite/testsite/__init__.py
    	new file:   testsite/testsite/asgi.py
    	renamed:    resources/site/settings.py -> testsite/testsite/settings.py
    	renamed:    resources/site/urls.py -> testsite/testsite/urls.py
    	new file:   testsite/testsite/wsgi.py

--- resources/test-template.py
+++ resources/test-template.py
@@ -4,67 +4,24 @@ import subprocess
 import time
 
 for cmd in ["docker container prune -f",
-            "docker image rm tmp-deleteme:latest site:latest -f"]:
+            "docker image rm site:latest -f"]:
   subprocess.check_call(cmd, shell=True)
-
-emptyDir = os.path.join(os.getcwd(), "emptydir")
-siteDir = os.path.join(os.getcwd(), "testsite")
-
-for d in [siteDir, emptyDir]:
-    if (os.path.isdir(d)):
-        shutil.rmtree(d)
-os.mkdir(emptyDir)
-
-# -------------------------------------
-print("Generating test django site...")
-# -------------------------------------
-user = subprocess.check_output(["id","-un"]).rstrip().decode("UTF-8")
-uid = subprocess.check_output(["id","-u"]).rstrip().decode("UTF-8")
-gid = subprocess.check_output(["id","-g"]).rstrip().decode("UTF-8")
-
-# We could just generate the site on the local machine, but doing it in a
-# container ensures that our version of django matches.
-subprocess.check_call(f"docker build -f environments/site/Dockerfile {emptyDir} -t tmp-deleteme --build-arg UNAME={user} --build-arg UID={uid} --build-arg GID={gid}", shell=True)
-
-siteGenerationCommands = ["cd /var/www",
-                          "pip install django",
-                          "django-admin startproject testsite",
-                          "cd /var/www/testsite",
-                          "python manage.py startapp testapp"]
-
-subprocess.check_call(f"docker run --user={uid}:{gid} -v {os.getcwd()}:/var/www tmp-deleteme bash -c \"{' && '.join(siteGenerationCommands)}\"", shell=True)
-
-
-# -------------------------------------
-print("Copying test site resources...")
-# -------------------------------------
-for f in ["settings.py", "urls.py"]:
-    shutil.copyfile(os.path.join("resources", "site", f), os.path.join("testsite", "testsite", f))
-
-for f in ["urls.py", "models.py", "views.py", "tests.py"]:
-    shutil.copyfile(os.path.join("resources","site",f"testapp-{f}"), os.path.join("testsite","testapp",f))
-
-templatesDir = os.path.join("testsite", "testapp", "templates")
-os.mkdir(templatesDir)
-for f in ["index.html", "base_generic.html"]:
-    shutil.copyfile(os.path.join("resources", "site", f), os.path.join(templatesDir, f))
-
-os.mkdir(os.path.join("testsite", "testapp", "static"))
-os.mkdir(os.path.join("testsite", "testapp", "static", "css"))
-shutil.copyfile(os.path.join("resources", "site", "styles.css"), os.path.join("testsite", "testapp", "static", "css", "styles.css"))
 
 # ------------------------------
 print("Updating environment...")
 # ------------------------------
+user = subprocess.check_output(["id","-un"]).rstrip().decode("UTF-8")
+uid = subprocess.check_output(["id","-u"]).rstrip().decode("UTF-8")
+gid = subprocess.check_output(["id","-g"]).rstrip().decode("UTF-8")
+
 # TODO: Why doesn't pointing to a file with env_file work in the docker-compose?
 environmentFile = ".env"
-if (os.path.exists(environmentFile)):
-    os.remove(environmentFile)
-shutil.copyfile(os.path.join("resources", "change.env"), environmentFile)
-with open(environmentFile, "a") as f:
-    f.write(f"UNAME={user}\n")
-    f.write(f"UID={uid}\n")
-    f.write(f"GID={gid}\n")
+if (not os.path.exists(environmentFile)):
+  shutil.copyfile(os.path.join("resources", "change.env"), environmentFile)
+  with open(environmentFile, "a") as f:
+      f.write(f"UNAME={user}\n")
+      f.write(f"UID={uid}\n")
+      f.write(f"GID={gid}\n")
 
 # -----------------------------
 print("Spawning containers...")
@@ -87,8 +44,6 @@ subprocess.check_call("python3 runtests.py", shell=True)
 print("Cleaning up...")
 # ---------------------
 subprocess.check_call("docker-compose down", shell=True)
-shutil.rmtree(emptyDir)
-shutil.rmtree(siteDir)
 os.remove(environmentFile)
 for cmd in ["docker container prune -f",
             "docker image rm tmp-deleteme:latest site:latest -f"]:
